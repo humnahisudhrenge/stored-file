d65ff440edc276c627726d093c972fb71967fd68
humnahisudhrenge <humnahisudhrenge13.7@gmail.com>
2024-05-13 16:59:06 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <code>Hidden😔</code>\n○ Language : <code>Python3</code>\n○ Library : <code>Pyrogram asyncio {__version__}</code>\n○ Source Code : <code>Not Available</code>\n○ Channel : NOT AVAILABLE\n○ Support Group : NOT AVAILABLE</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
